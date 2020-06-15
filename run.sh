0b6a41cf6898b625df20ce3c12d5f71e23efa3db
Reto Hablützel <rethab@protonmail.ch>
2020-06-15 10:14:38 +0200

    make run script flexible

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-javac Add.java && hexdump -v -e '/1 "%01u "' Add.class  | awk -f jvm.awk
+file=$1
+if [[ ! -f "$file.java" ]];
+then
+  echo "$file.java is not a file"
+  exit 1
+fi
+
+javac "$file.java" && hexdump -v -e '/1 "%01u "' "$file.class"  | awk -f jvm.awk
